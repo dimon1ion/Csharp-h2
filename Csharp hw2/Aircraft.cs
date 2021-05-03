3a46d22b166caf88974629cdcf8a90e794251715
Dimon <Dimon@DIMON>
2021-05-03 21:43:58 +0400

    Add project files.

---
+++ Csharp hw2/Aircraft.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Csharp_hw2
+{
+    partial class Aircraft
+    {
+        #region Properties
+
+        public string Name { get; set; }
+        public int BodyLength { get; set; }
+        public int WingLength { get; set; }
+        public int Wheels { get; set; }
+        public int Power { get; set; }
+        public string Sound { get; set; }
+        public int Passangers { get; set; }
+
+        #endregion
+
+        public static int aircrafts;
+        public static int destroyedAircrafts;
+
+        #region Constructors
+
+        static Aircraft()
+        {
+            aircrafts = 0;
+            destroyedAircrafts = 0;
+        }
+        public Aircraft()
+        {
+            Name = "Noname";
+            BodyLength = 0;
+            WingLength = 0;
+            Wheels = 0;
+            Power = 0;
+            Sound = "Wwwwwwww..";
+            Passangers = 0;
+            aircrafts++;
+        }
+        public Aircraft(string name, int bodyLength, int wingLength, int wheels, int power, string sound, int passangers)
+        {
+            Name = name;
+            BodyLength = bodyLength;
+            WingLength = wingLength;
+            Wheels = wheels;
+            Power = power;
+            Sound = sound;
+            Passangers = passangers;
+            aircrafts++;
+        }
+
+        #endregion
+
+        #region Metods
+
+        public Aircraft Takeoff()
+        {
+            Console.Clear();
+            Console.WriteLine($"Aircraft \"{Name}\" is taking off {Sound}");
+            Thread.Sleep(10000);
+            Console.WriteLine($"Aircraft \"{Name}\" taked off");
+            return this;
+        }
+        public Aircraft Land()
+        {
+            Console.Clear();
+            Console.WriteLine($"Aircraft \"{Name}\" is landing {Sound}");
+            Thread.Sleep(10000);
+            Console.WriteLine($"Aircraft \"{Name}\" landed");
+            return this;
+        }
+        public Aircraft BoardingPassangers()
+        {
+            Console.Clear();
+            Console.WriteLine("Boarding Passengers..");
+            Console.Write("Passenger number:");
+            for (int i = 1; i <= Passangers; i++)
+            {
+                Console.SetCursorPosition(18, 2);
+                Console.WriteLine("                                ");
+                Console.SetCursorPosition(18, 2);
+                Console.WriteLine($"{i} / {Passangers}");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            return this;
+        }
+
+        #endregion
+    }
+}
---
+++ Csharp hw2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_hw2
+{
+    class Program
+    {
+        static int SumPassengers(ref Aircraft a1, ref Aircraft a2)
+        {
+            return a1.Passangers + a2.Passangers;
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Aircraft[] aircrafts =
+            {
+                new Aircraft("DimOn", 100, 45, 6, 10000, "Wrum wrum..", 150),
+                new Aircraft(),
+                new Aircraft(),
+                new Aircraft(),
+                new Aircraft()
+            };
+            aircrafts[0].BoardingPassangers().Takeoff().Land();
+            Aircraft.Destroy();
+            Console.WriteLine($"Aircrafts: {Aircraft.aircrafts}" +
+                $"\nDestroyed: {Aircraft.destroyedAircrafts}");
+            
+        }
+    }
+}
